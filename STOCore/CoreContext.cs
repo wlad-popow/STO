b493a80079e7f87ed087c241504a1f27f2e22783
wlad <wlad-popow@mail.ru>
2016-12-08 00:23:35 +0600

    add core

---
+++ STOCore/CoreContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STOCore
+{
+    class CoreContext : DbContext
+    {
+        public CoreContext()
+            : this("STOEntities")
+        {
+        }
+
+        public CoreContext(string connectionString)
+            : base(connectionString)
+        {
+            Configuration.AutoDetectChangesEnabled = false;
+            Configuration.LazyLoadingEnabled = false;
+            Configuration.ProxyCreationEnabled = false;
+        }
+
+        protected override void OnModelCreating(DbModelBuilder mb)
+        {
+        }
+    }
+}
